1799146f843db6bd6e99cb923b61a9f23dc7dfa1
DarabMonib <darab.monib@invozone.com>
2022-10-04 17:12:41 +0500

    .env.example updated

--- auth/passport.js
+++ auth/passport.js
@@ -1,15 +1,12 @@
 const passport = require("passport");
-
+require("dotenv").config({ path: "../.env" });
 const SlackStrategy = require("passport-slack-oauth2").Strategy;
-
-const SLACK_CLIENT_ID = "4161180073968.4137388626883";
-const SLACK_CLIENT_SECRET = "4b9cce3edb3c01e8a303b4493aba2ce9";
 
 passport.use(
   new SlackStrategy(
     {
-      clientID: SLACK_CLIENT_ID,
-      clientSecret: SLACK_CLIENT_SECRET,
+      clientID: process.env.SLACK_CLIENT_ID,
+      clientSecret: process.env.SLACK_CLIENT_SECRET,
       skipUserProfile: false,
       scope: [
         "identity.basic",
