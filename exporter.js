94084524316f85a675de90b0476a90e6cd438611
Zain-Ali <zain.ali@invozone.com>
2022-10-18 16:03:35 +0500

    Posting Data Functionality Added

--- exporter.js
+++ exporter.js
@@ -1,8 +1,16 @@
 require("dotenv").config();
 
 const { WebClient } = require("@slack/web-api");
+
+const axios = require("axios");
+const https = require("https");
+const FormData = require("form-data");
+const fs = require("fs");
+
 const token = process.env.SLACK_USER_TOKEN;
 const web = new WebClient(token);
+const BASEURL = "https://source-im.invo.zone";
+const ACCESSTOKEN = "huokuoz633yupcqi9zti4ywdna";
 
 const slackChannelsModel = require("./models").SlackChannel;
 
@@ -46,7 +54,7 @@ const findChannels = async (req, res) => {
         "type",
         "membersCount",
         "creationDate",
-        "status"
+        "status",
       ],
     });
     res.status(200).json({ data: allChannels });
@@ -99,7 +107,7 @@ const syncHistroy = async (req, res) => {
       },
     });
     await channelRecord.update({ status: "Pending" });
-    fetchAllMessageWithTreads(req);
+    fetchAllMessageWithTreads(req, res);
     res.status(200).json({ data: "Syncing start successfully!" });
   } catch (error) {
     console.error(error);
@@ -123,6 +131,7 @@ const fetchAllMessageWithTreads = async (req, res) => {
     // Complete replies being stored into an array.
     let allReplies = [];
     // Cursor changes overtime as new requests update it.
+    let rootIds = [];
     let cursor = req.body.cursor || null;
     const channelRecord = await slackChannelsModel.findOne({
       where: {
@@ -137,19 +146,140 @@ const fetchAllMessageWithTreads = async (req, res) => {
       cursor,
       channelRecord
     );
+
+    for (let i = allMessages.length - 1; i >= 0; i--) {
+      const username = await web.users.info({
+        user: allMessages[i].user,
+      });
+      if (allMessages[i].files) {
+        console.log("going in files function...");
+        const fetchedFiles = await fetchFromSlack(allMessages[i].files);
+        if (fetchedFiles.length >= 1) {
+          const sendFile = await postFilesToMettermost(
+            fetchedFiles,
+            allMessages[i].text,
+            username.user.real_name,
+            channelRecord.mattermostId
+          );
+        }
+      } else {
+        let singleMessage = await axios.post(BASEURL + "/hooks/16988a5j1pbabpdyxfiogh6o4h",
+          {
+            text: allMessages[i].text,
+            normal_hook: true,
+            username: username.user.real_name,
+            channel: channelRecord.mattermostId,
+          }
+        );
+        if (allMessages[i].thread_ts) {
+          rootIds.push({ ts: allMessages[i].thread_ts, rootId: singleMessage.data.id })
+        }
+      }
+    }
+
+    for (let i = allReplies.length - 1; i >= 0; i--) {
+      for (let x = 1; x < allReplies[i].length; x++) {
+        const username = await web.users.info({
+          user: allReplies[i][x].user,
+        });
+        const rootId = rootIds.find((item) => allReplies[i][x].thread_ts === item.ts).rootId;
+        console.log("This is root for this thread", rootId);
+        if (allReplies[i][x].files) {
+          console.log("going in files function for thread...");
+          const fetchedFiles = await fetchFromSlack(allReplies[i][x].files);
+          if (fetchedFiles.length >= 1) {
+            const sendFile = await postFilesToMettermost(
+              fetchedFiles,
+              allReplies[i][x].text,
+              username.user.real_name,
+              channelRecord.mattermostId,
+              rootId
+            );
+          }
+        } else {
+          const singleReply = await axios.post(BASEURL + "/hooks/16988a5j1pbabpdyxfiogh6o4h",
+            {
+              root_id: rootId,
+              text: allReplies[i][x].text,
+              normal_hook: true,
+              username: username.user.real_name,
+              channel: channelRecord.mattermostId,
+            }
+          );
+        }
+      }
+    }
+
     const date = new Date();
     await channelRecord.update({ lastUpdatedAt: date, status: "Completed" });
     const socketPayload = {
-      lastUpdatedAt : date,
-      channelId : channelId
-    }
-    const io = req.app.get('socketio');
+      lastUpdatedAt: date,
+      channelId: channelId,
+    };
+    const io = req.app.get("socketio");
     io.emit("lastUpdated", socketPayload);
     return;
-    // res.status(200).json({ messages: allMessages, replies: allReplies });
   } catch (error) {
-    console.error(error);
+    console.error('this is error -> ', error);
     res.status(500).json({ messages: "Internal Server Error", error: error });
+  }
+};
+
+const fetchFromSlack = async (file) => {
+  const filesFromSlack = [];
+  for (let i = 0; i < file.length; i++) {
+    if (file[i].url_private_download && typeof(file[i].url_private_download) !== 'undefined') {
+      const fetchFile = await axios.get(file[i].url_private_download, {
+        headers: { Authorization: "Bearer " + process.env.SLACK_USER_TOKEN },
+      });
+      await filesFromSlack.push(fetchFile);
+    }
+  }
+  return filesFromSlack;
+};
+
+const postFilesToMettermost = async (
+  fileCollection,
+  textMessage,
+  username,
+  mattermostId,
+  rootId = null
+) => {
+  try {
+    const URLsite = BASEURL + "/api/v4/files";
+    let formData = new FormData();
+    console.log(fileCollection?.length, "POSTING TO MATTERMOST!");
+    fileCollection.map((file) => {
+      formData.append("files", file);
+    });
+    formData.append("Authorization", "Bearer " + ACCESSTOKEN);
+    formData.append("channel_id", "mjkhchcykidofe9ncgtzbge3ec");
+    console.log("formData 2 -> ", formData);
+    // All posted files will be received in the response
+    let responseData = await axios.post(URLsite, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+        Authorization: "Bearer " + ACCESSTOKEN,
+      },
+    });
+    // Will stay 0
+    const postIds = responseData?.data?.file_infos?.map((el) => el.id);
+    console.log("This is postIds -> ", postIds);
+    // After posting multiple files, we need to
+    const response = await axios.post(BASEURL + "/hooks/16988a5j1pbabpdyxfiogh6o4h",
+      {
+        root_id: rootId ? rootId : null,
+        text: textMessage,
+        normal_hook: true,
+        username: username,
+        channel: mattermostId,
+        file_ids: postIds,
+      }
+    );
+    console.log("This is images response -> ", response);
+    return response;
+  } catch (error) {
+    console.log("Error, please have a look at ", error);
   }
 };
 
